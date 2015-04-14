788cb60288fe65a53ae84f0080d62f386865f730
이의재 <astroluj@gmail.com>
2015-04-15 02:56:28 +0900

    Refactoring Complete

--- MobileLap_01_Calculator/gen/com/example/mobilelap_01_calculator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.mobilelap_01_calculator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- MobileLap_02_SimpleTour/gen/com/example/mobilelap_02_simpletour/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.mobilelap_02_simpletour;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
